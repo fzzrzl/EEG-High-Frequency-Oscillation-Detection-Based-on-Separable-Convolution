3cb92710cd64ed8fdba316208516b8866cdb99eb
fzzrzl <zhanglong321211@gmail.com>
2024-12-18 15:47:11 +0800

    Update get_results.py

--- get_results.py
+++ get_results.py
@@ -1,7 +1,10 @@
 from test import *
-
+import argparse
 if __name__ == "__main__":
-    model_path = "model_conv_kernel17_9459.pth"
-    test_data_dir = "test/"
-    framework = "pytorch"
-    evaluation(model_path, test_data_dir, framework, device="cuda:0")
+    parser = argparse.ArgumentParser(description='HFO Detection')
+    parser.add_argument('--test_path', type=str, default=r'C:\EEG-project\ISICDM 2024\test\*.csv', help='path to dataset directory')
+    parser.add_argument('--save_path', type=str, default='model_conv.pth', help='path to save the model')
+    parser.add_argument('--framework', type=str, default="pytorch", help='framework')
+    parser.add_argument('--device', type=str, default="device", help='cuda:0')
+    args = parser.parse_args()
+    evaluation(args.model_path, args.test_data_dir, args.framework, device=args.device)
