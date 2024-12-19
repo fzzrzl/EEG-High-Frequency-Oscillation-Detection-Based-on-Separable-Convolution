d4d4736616a557ffa2a0d454918595402a0fc742
fzzrzl <zhanglong321211@gmail.com>
2024-12-19 15:53:18 +0800

    Update get_results.py

--- get_results.py
+++ get_results.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     parser.add_argument('--test_path', type=str, default=r'/home/test/', help='path to dataset directory')
     parser.add_argument('--save_path', type=str, default='model_conv.pth', help='path to save the model')
     parser.add_argument('--framework', type=str, default="pytorch", help='framework')
-    parser.add_argument('--device', type=str, default="device", help='cuda:0')
+    parser.add_argument('--device', type=str, default="cuda:0", help='device')
     args = parser.parse_args()
-    evaluation(args.model_path, args.test_data_dir, args.framework, device=args.device)
+    evaluation(args.save_path, args.test_path, args.framework, device=args.device)
